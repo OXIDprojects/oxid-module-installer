f24aa918d44cc91d5b09e53e2b5f1357776815a0
keywan <keywan.ghadami@oxid-esales.com>
2018-12-19 07:35:03 +0100

    initial metadata file

---
+++ metadata.php
@@ -0,0 +1,21 @@
+<?php
+$sMetadataVersion = '2.1';
+$aModule = [
+    'id'          => 'moduleinstaller',
+    'title'       => 'OXID Community Module Installer',
+    'description' => 'OXID Community Module Installer',
+    'thumbnail'   => '',
+    'version'     => '0.0.0',
+    'author'      => 'OXID Community',
+    'url'         => 'https://github.com/OXIDprojects/oxid-module-installer',
+    'email'       => '',
+    'extend'      => [
+    ],
+    'controllers' => [
+    ],
+    'templates'   => [
+    ],
+    'settings'    => [
+    ]
+];
+
