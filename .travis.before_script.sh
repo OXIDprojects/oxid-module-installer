c39f65518bac67218df9d348bffc222e563a2d86
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2018-12-19 14:41:18 +0100

    update travis config (#6)
    update travis config (using "travis" as repository name to avoid confusion when using config together with other modules, using new version of oxid, added debug commands that can be used to clear cache on the travis server (because cache seams to be persistent)

--- .travis.before_script.sh
+++ .travis.before_script.sh
@@ -3,7 +3,7 @@
 cd ~/
 mkdir OXID
 cd OXID
-composer create-project oxid-esales/oxideshop-project . dev-b-6.0-ce
+composer create-project oxid-esales/oxideshop-project . dev-b-6.1-ce
 sed -i -e "s@<dbHost>@127.0.0.1@g" source/config.inc.php
 sed -i -e "s@<dbName>@oxid@g" source/config.inc.php
 sed -i -e "s@<dbUser>@root@g" source/config.inc.php
@@ -15,5 +15,11 @@ sed -i -e "s@partial_module_paths: null@partial_module_paths: fp/debugbar@g" tes
 sed -i -e "s@run_tests_for_shop: true@run_tests_for_shop: false@g" test_config.yml
 
 # Registrieren
-composer config repositories.oxid-community/moduleinstaller path ${TRAVIS_BUILD_DIR}
+
+# Registrieren
+composer config repositories.travis path ${TRAVIS_BUILD_DIR}
+#if composer fails to install the following commands may help to get things running:
+#composer config repo.packagist false & composer clear-cache
+composer config minimum-stability dev
+
 composer require "oxid-community/moduleinstaller:*"
