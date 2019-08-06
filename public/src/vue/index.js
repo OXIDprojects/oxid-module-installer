f7af5308b0b82455e4b820547e7ba79ec62a601f
Sascha Weidner <=>
2019-08-06 08:06:30 +0200

    alpha layout

--- public/src/vue/index.js
+++ public/src/vue/index.js
@@ -4,6 +4,17 @@ import { store } from './_store';
 import { router } from './_helpers';
 import App from './app/App';
 
+import Header from './Components/Header_footer/Header.vue'
+import Footer from './Components/Header_footer/Footer.vue';
+
+import Search from './Components/Search.vue';
+import Packages from './Components/Packages/Packages.vue';
+
+Vue.component('app-header', Header)
+Vue.component('app-footer', Footer)
+Vue.component('app-search', Search)
+Vue.component('app-packages', Packages)
+
 new Vue({
     el: '#app',
     router,
