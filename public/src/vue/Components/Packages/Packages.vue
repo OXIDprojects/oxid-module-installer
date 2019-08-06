25f7e187e0503bcc1331f38ff972f966b4031ae9
Sascha Weidner <=>
2019-08-06 12:29:07 +0200

    Show installed packages, grouped by oxid-modules and others.

--- public/src/vue/Components/Packages/Packages.vue
+++ public/src/vue/Components/Packages/Packages.vue
@@ -1,6 +1,26 @@
 <template>
     <div id="packages">
-        PACKAGES
+        <h2>Oxid Packages</h2>
+
+        <div 
+            class="oxid-packages"
+            v-for="(oxid_package,index) in packages.oxid"
+            :key="index"
+        >
+            <h3>{{oxid_package.name}}</h3>
+            <p>Version: {{oxid_package.version}}</p>
+        </div>
+
+        <h2>Other Packages</h2>
+
+        <div 
+            class="oxid-packages"
+            v-for="(other_package,other) in packages.other"
+            :key="other"
+        >
+            <h3>{{other_package.name}}</h3>
+            <p>Version: {{other_package.version}}</p>
+        </div>
     </div>
 </template>
 
@@ -8,14 +28,14 @@
 export default {
     data() {
         return {
-
+            packages: {oxid:null,other:null}
         };
     },
     methods: {
         fetchPackages() {
             this.$http.get('/oxid/moduleinstaller/packages/')
                 .then( response => {
-                    console.log(response)
+                    this.packages = response.body.packages
                 })
         }
     },
