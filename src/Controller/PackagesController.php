8cd0a9e9979f8fd0d83e4ff49ad4181512ffe427
Sascha Weidner <=>
2019-08-22 07:47:19 +0200

    First working prototype to update all or a selection of packages!

--- src/Controller/PackagesController.php
+++ src/Controller/PackagesController.php
@@ -4,14 +4,18 @@ declare (strict_types = 1);
 
 namespace OxidCommunity\ModuleInstaller\Controller;
 
-use Composer\Console\Application;
+use OxidCommunity\ModuleInstaller\Composer\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
 use OxidCommunity\ModuleInstaller\Composer\ComposerApi;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Console\Output\BufferedOutput;
-
+use OxidCommunity\ModuleInstaller\Composer\Command\UpdateCommand;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Helper\HelperSet;
+use Symfony\Component\Console\Output\ConsoleOutput;          
+use Composer\IO\ConsoleIO;
 
 class PackagesController extends Controller
 {
@@ -45,8 +49,10 @@ class PackagesController extends Controller
 
     public function updateAction()
     {
+        $Output = $this->composerUpdate(new ComposerApi());
         return new JsonResponse([
-            'status' => 'update package'
+            'status' => 'update package',
+            'run' => stream_get_contents(fopen('php://stdin', 'r'), 1)
         ]);
     }
 
@@ -72,19 +78,46 @@ class PackagesController extends Controller
             }
         }
 
-        $composerApi->setEnv();
-
-        // call `composer update` command programmatically
-        // $Input = new ArrayInput(array('command' => 'update'));
-        // $Application = new Application();
-        // $Application->setAutoExit(false);
-        // $Output = new BufferedOutput();
-        // $Application->run($Input, $Output);
+        $this->composerUpdate($composerApi);
 
         return new JsonResponse([
             'status' => 'update selected package',
             // 'run' => $Output->fetch()
         ]);
+    }
+
+    private function composerUpdate(ComposerApi $composerApi)
+    {
+        ini_set('memory_limit', '4G');
+        ini_set('max_execution_time', '0');
+        $composerApi->setEnv();
+
+        $input = new ArgvInput(array());
+        $output = new ConsoleOutput();
+        $helper = new HelperSet();
+        $io = new ConsoleIO($input, $output, $helper);
+
+        // die("<pre>" . __METHOD__ .":\n" . print_r($input, true));
+        chdir($composerApi->getRootPath());
+        $update = new UpdateCommand();
+        $update->setComposer($composerApi->getComposer($io));
+        $update->run($input, $output);
+
+        return $output->getStream();
+
+        
+        
+        $Input = new \Symfony\Component\Console\Input\StringInput('update -vvv -n --no-dev --prefer-dist ');
+        $Output = new \Symfony\Component\Console\Output\StreamOutput(fopen('php://output','w'));
+        
+
+        $Application = new Application();
+        $Application->setComposer($composerApi->getComposer());
+        $Application->setAutoExit(false);
+        // $Output = new BufferedOutput();
+        $Application->run($Input, $Output);
+
+        return $Output;
     }
 
     public function deleteAction()
